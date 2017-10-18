4203a318399abd7f222dbe34a47063e9fd27fed5
Oskar <oskarbank@gmail.com>
2017-10-18 13:28:03 +0200

    test

---
+++ src/Task3Bonus.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Task3Bonus {
+    public static void main(String[] args) {
+        int[] someNumbers = {5,5,1,1,4,4,4,4};
+      //  1 1 1 1 1 1 1 4 5 5
+      //          1 4 5 5 5 5 5 5 5 5
+        Arrays.sort(someNumbers);
+
+
+        for (int i = 0; i < someNumbers.length; i++) {
+            System.out.println(someNumbers[i]);
+        }
+
+        System.out.println("-------------------");
+        
+        int candidat = (someNumbers.length / 2) + 1;
+        System.out.println(candidat);
+        
+       int counter = 0;
+        for(int i = 0; i < someNumbers.length; i++){
+            if(someNumbers[i] == someNumbers[candidat]){
+                counter++;
+            }
+        }
+
+        if(counter >= candidat) {
+            System.out.println("Czy tablica ma Krola? " + "TAK");
+        }
+    }
+}
